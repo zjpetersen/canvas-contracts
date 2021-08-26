87f3aab10dacfe7d2bf52611e789c0f8996c6f05
ZachPetersen <zjpetersen12@gmail.com>
2021-08-25 22:34:53 -0500

    Add comments

--- contracts/MosaicTiles.sol
+++ contracts/MosaicTiles.sol
@@ -2,44 +2,42 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-//Rename to MosaicTiles, get rid of MosaicMarket
+/**
+  @dev ERC721, Non-Fungible Token, implementation.  There are 7056
+    available NFTs.  Includes functionality to mint new tiles as well
+    as to update the color of the tile.
+ */
 contract MosaicTiles is ERC721 {
     address approvedMarket;
     address admin;
+    uint maxColorSize;
 
+    /**
+     * @dev Emitted when `owner` changes the `tokenId` color to `updatedColor`.
+     */ 
     event ColorBytesUpdated(uint tokenId, address owner, bytes updatedColor);
 
     constructor(uint[] memory tokenIds) ERC721("Tiles", "TIL") {
         admin = msg.sender;
+        maxColorSize = 5000;
         _setInitialTiles(tokenIds); //Set initial tiles
     }
 
-    //Trading functionality is extracted out to a separate contract.
-    //If an issue ever comes up in the trading functions, this can be used to update the contract.
-    // function addApprovedMarket(address market) external {
-    //     require(msg.sender == admin);
-    //     approvedMarket = market;
-    // }
-
-    //Override approve method to allow the market as an approver
-    // function approve(address to, uint256 tokenId) public virtual override {
-    //     address owner = ERC721.ownerOf(tokenId);
-    //     require(to != owner, "ERC721: approval to current owner");
-
-    //     require(
-    //         _msgSender() == approvedMarket || _msgSender() == owner || isApprovedForAll(owner, _msgSender()),
-    //         "ERC721: approve caller is not owner nor approved for all"
-    //     );
-
-    //     _approve(to, tokenId);
-    // }
+    /**
+     * @dev Mints the initial set of tiles on contract creation.
+     */
     function _setInitialTiles(uint[] memory tokenIds) private {
         require(tokenIds.length > 0 && msg.sender == admin);
         for (uint i = 0; i < tokenIds.length; i++) {
-            mintTile(tokenIds[i]);
+            require(_isValidToken(tokenIds[i]));
+            _safeMint(msg.sender, tokenIds[i]);
         }
     }
 
+    /**
+     * @dev Returns the owner of the tile, or the empty address if no
+     * owner exists 
+     */
     function getOwner(uint tokenId) external view returns (address) {
         require(_isValidToken(tokenId));
 
@@ -50,8 +48,10 @@ contract MosaicTiles is ERC721 {
         }
     }
 
-    //Can be called directly or via a trading contract
-    //Emits transfer
+    /**
+     * @dev Mints new tiles for all `tokenIds` if it doesn't already exist.
+     * Emits a {Transfer} event.
+     */
     function mintTiles(uint[] memory tokenIds) public {
         require(tokenIds.length > 0 && tokenIds.length <= 20);
         require(balanceOf(msg.sender) + tokenIds.length <= 100);
@@ -61,18 +61,31 @@ contract MosaicTiles is ERC721 {
         }
     }
 
-    //Can be called directly or via a trading contract
-    //Emits a transfer
+    /**
+     * @dev Mints new tile for `tokenId` if it doesn't already exist.
+     * Emits a {Transfer} event.
+     */
     function mintTile(uint tokenId) public {
         require(balanceOf(msg.sender) < 100);
         require(_isValidToken(tokenId));
         _safeMint(msg.sender, tokenId);
     }
 
+    /**
+     * @dev Overriding URI from {ERC721-_baseURI}.  Used to display the NFT metadata.
+     */
     function _baseURI() override internal pure returns (string memory) {
-        return "localhost:4000/tile";
+        return "localhost:4000/tile/metadata/";
     }
 
+    /**
+     * @dev Sets the color for all `tokenIds`.  `colors` will be updated for `tokenIds` with the same index.
+     * Setting the color is actually just emitting an event.
+     * For example tokenIds=[10,50] and colors=[x,y] would emit events 
+     * ColorBytesUpdated(10, <msg.sender>, x) and ColorBytesUpdated(50, <msg.sender>, y)
+     *
+     * Requires tokenIds and colors to be the same length.
+     */
     function setColorBytesBulk(uint[] memory tokenIds, bytes[] memory colors) external {
         require(tokenIds.length > 0 && tokenIds.length == colors.length, "tokenIds length must match colors length");
         for (uint i = 0; i < tokenIds.length; i++) {
@@ -80,15 +93,27 @@ contract MosaicTiles is ERC721 {
         }
     }
 
+    /**
+     * @dev Sets the `color` for `tokenId`
+     * Setting the color is actually just emitting an event.
+     * Does a sanity check to ensure people aren't uploading full size images or other large data.
+     * Full validation of the color byte array is handled off chain to ensure it is a png, jpg, or gif.
+     *
+     * Emits a {ColorBytesUpdated} event
+     */
     function setColorBytes(uint tokenId, bytes memory color) public {
-        require(_isValidToken(tokenId), "TokenId is too high");
+        require(_isValidToken(tokenId), "TokenId is invalid");
         require(_isOwner(tokenId, msg.sender), "Sender is not the token owner");
-        //TODO validate color string, also some check on color size.
+
+        require(color.length < maxColorSize, "Color array is too big, please use 16x16 image");
 
         emit ColorBytesUpdated(tokenId, msg.sender, color);
     }
 
 
+    /**
+     * @dev Check if `tokenId` exists.
+     */
     function exists(uint tokenId) public view returns (bool) {
         require(_isValidToken(tokenId));
 
